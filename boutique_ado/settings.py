f40ac601d1537eb516331e48a5d68c267e15775e
Brandon Collins <bpcollins1995@gmail.com>
2023-12-31 14:12:28 +0000

    typo

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -187,7 +187,7 @@ if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-bp' #might be wrong check
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     #Static an meda files
